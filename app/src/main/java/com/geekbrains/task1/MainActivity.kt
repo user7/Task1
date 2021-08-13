ff3d29da9d5a047f40136f76ab5e71bd8e856385
svr <svr@protonmail.com>
2021-08-13 14:28:27 +0300

    task 1.2

--- app/src/main/java/com/geekbrains/task1/MainActivity.kt
+++ app/src/main/java/com/geekbrains/task1/MainActivity.kt
@@ -2,10 +2,12 @@ package com.geekbrains.task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener { setContentView(R.layout.layout2) }
     }
 }
